06c7f4a64255f6b3f66788137184f51d59569bb3
uDanielBispo <danielbispo3015@hotmail.com>
2021-06-21 15:46:44 -0300

    Exercicio de Contador primeiro codigo me acostumando a escrever em ingles

---
+++ Let'sCount/Let'sCount.js
@@ -0,0 +1,43 @@
+function Count()
+{
+    let GetStart = document.getElementById('Start');
+    let GetEnd = document.getElementById('End');
+    let GetStep = document.getElementById('Step');
+    let GetAnswer = document.getElementById('Answer');
+
+    let Start = Number(GetStart.value);
+    let End = Number(GetEnd.value);
+    let Step = Number(GetStep.value);
+    let CounterStart = Start;
+
+    Answer.innerHTML += `Contando: <br>`;
+   
+    //Fill Condition
+    if(GetStart.value.length != 0 && GetEnd.value.length != 0 && GetStep.value.length != 0)
+    {
+        //Step cant be 0
+        if(Step != 0)
+        {
+            //Growing Condition
+            if(Start < End)
+            {                                 
+                for (CounterStart; CounterStart <= End; CounterStart += Step)
+                {
+                    Answer.innerHTML += `\u{1F449} ${CounterStart} `;
+                }
+            }
+            //Decreasing Condition
+            if(Start > End)
+            {
+                for (CounterStart; CounterStart >= End; CounterStart -= Step)
+                {
+                    Answer.innerHTML += `\u{1F449} ${CounterStart} `;
+                }
+            }
+        }
+        else{alert("Step can't be 0")}
+    }
+    else{alert('Fill in the gaps.')}
+
+    Answer.innerHTML += `\u{1F3C1}`;
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AmbienteJavaScript
 
-Estudos de JS, HTML e CSS
+Estudos de JS, HTML e CSS. Usando exercicios propostos pelo canal
+Curso em Vídeo
 
 Me acostumando a escrever códigos em inglês
